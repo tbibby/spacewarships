400493b024e9cc7e9ed5affed5ae2e90ce65c2ad
Thomas Bibby <thomas@bibby.ie>
2017-04-09 11:15:37 +0100

    Initial Commit

---
+++ SpaceWarShips/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpaceWarShips
+//
+//  Created by Thomas Bibby on 09/04/2017.
+//  Copyright © 2017 Till Now Limited. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
